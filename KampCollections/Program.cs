be10bd2283fec5d7cb7dfb8e46bb9e5f7071c2e3
ceyhunonac <74990452+ceyhunonac@users.noreply.github.com>
2021-01-20 23:50:55 +0300

    Add project files.

---
+++ KampCollections/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KampCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[] { "ceyhun", "engin", "ediz" };
+            //console.writeline(isimler[0]);
+            //console.writeline(isimler[1]);
+            //console.writeline(isimler[2]);
+
+            //isimler = new string[5];
+            //isimler[3] = "ilker";
+            //console.writeline(isimler[3]);
+            //console.writeline(isimler[0]);
+
+
+            List<string> isimler2 = new List<string> { "Ceyhun", "Engin", "Ediz","Sefa" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            isimler2.Add("İlker");
+            Console.WriteLine(isimler2[4]);
+            Console.WriteLine(isimler2[0]);
+        }
+    }
+}
